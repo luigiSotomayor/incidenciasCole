84db9804cd77c0d3b889224305db83b59e5429e8
Luis Sotomayor <lsbvks77@gmail.com>
2026-05-18 00:25:20 +0200

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import { supabase } from './supabase'
 
 function App() {
+  const [clientes, setClientes] = useState([])
+
+  useEffect(() => {
+    getClientes()
+  }, [])
+
+  async function getClientes() {
+    const { data, error } =
+      await supabase
+        .from('clientes')
+        .select('*')
+
+    if (!error) setClientes(data)
+  }
 
   return (
     <>
-      <h1>Incidencias Alumnos</h1>
+      {clientes.map(c => (
+        <p key={c.id}>{c.nombre}</p>
+      ))}
     </>
   )
 }
